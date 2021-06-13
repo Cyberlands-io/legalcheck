1acdc6f972f9d02cedc437168f0a40dffe5bc620
excellencenatural <choosefolkvangrorwalhall@gmail.com>
2021-06-13 13:03:46 +0300

    updateLocalSearch

--- git.py
+++ git.py
@@ -1,9 +1,14 @@
 import requests
 from abc import ABC, abstractmethod
 from colorama import Fore, Style
+from parse import compile
+import os
+import re
 
 
 class License():
+
+    TEMPLATES_PATH = './templates'
 
     def __init__(self, name, permissions, conditions, limitations) -> None:
         self.name = name
@@ -11,29 +16,21 @@ class License():
         self.conditions = conditions
         self.limitations = limitations
         self.get_max_len()
-    
-    licenses = { 
-                 "apache-2.0" : "Apache",
-                 "bsd-2-clause" : "BSD 2-Clause",
-                 "bsd-3-clause" : "BSD 3-Clause",
-                 "bsl-1.0" : "Boost Software",
-                 "cc0-1.0" : "Creative Commons Legal Code",
-                 "epl-2.0" : "Eclipse Public",
-                 "gpl-3.0" : "GNU GENERAL PUBLIC",
-                 "agpl-3.0" : "GNU AFFERO GENERAL",
-                 "lgpl-2.1" : "GNU LESSER GENERAL PUBLIC",
-                 "mit" : "MIT",
-                 "mpl-2.0" : "Mozilla Public",
-                 "unlicense" : "free and unencumbered software released"
-                 }
 
     @staticmethod
-    def compare_license(path_to_license_file):
-        with open(path_to_license_file) as f:
-            datafile = f.read()
-            for key, value in License.licenses.items():
-                if value.lower() in datafile.lower():
-                    return GitHub.get_license_by_key(key)
+    def analize_license(path_to_license_file):
+        
+        pattern = r"[\n\t\s]*"
+        for f in os.listdir(License.TEMPLATES_PATH):
+            with open(f'{License.TEMPLATES_PATH}/{f}','r') as template:
+                p = compile(re.sub(pattern, "",template.read()))
+                with open(path_to_license_file,'r') as license_file:
+                    l = re.sub(pattern, "", license_file.read())              
+                    res = p.parse(l)
+                    if res:
+                        return GitHub.get_license_by_key(f[:-4])
+
+            
 
     def print_license_data(self):
         print('\n{}{: >40}\n'.format(Style.BRIGHT + Fore.YELLOW, self.name))
@@ -49,32 +46,30 @@ class License():
                                                          self.index_is_exists(self.limitations, index)))
 
     def get_max_len(self):
-        self.max_len = \
-            max({'permissions': self.permissions, 'conditions': self.conditions,
-                 'limitations': self.limitations}.items(),
-                key=len)[0]
+        self.max_len = max({'permissions': self.permissions, 'conditions': self.conditions,'limitations': self.limitations}.items(), key=len)[0]
 
     def index_is_exists(self, data, index):
         return data[index] if 0 <= index < len(data) else " "
-    
-    
 
 
 class Repository(License):
     
-
-    def __init__(self, rep_license=None, name='', owner='', url='') -> None:
+    def __init__(self, rep_license=None, name='', owner='', url=''):
         self.name = name
         self.owner = owner
         self.url = url
         self.rep_license = rep_license
 
-    def print_license_data(self, rep):
-        print('\n{}{}'.format(Style.BRIGHT + Fore.MAGENTA,self.url if self.url else self.name))
-        if rep.rep_license:
-            rep.rep_license.print_license_data()
+    @staticmethod
+    def print_repository_data(repository):
+        print('\n{}{}'.format(Style.BRIGHT + Fore.MAGENTA,repository.url if repository.url else repository.name))
+        if repository.rep_license:
+            repository.rep_license.print_license_data()
         else:
             print('\n{}{}'.format(Style.BRIGHT + Fore.YELLOW,'Private or None License!'))
+
+
+# Gits
 
 class Git(ABC):
 
@@ -85,14 +80,16 @@ class Git(ABC):
 
 class GitHub(Git, Repository):
 
+    def __init__(self, name, owner, url):
+        repository_license = self.get_repository_license(owner, name)
+        super().__init__(repository_license, name, owner, url)
+
     def get_repository_license(self, repository_owner, repository_name):
-        response = requests.get(f'https://api.github.com/repos/{repository_owner}/{repository_name}/license',
-                                headers={"Authorization": "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"})
+
+        response = requests.get(f'https://api.github.com/repos/{repository_owner}/{repository_name}/license', headers={"Authorization": "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"})
         if response.status_code == 200:
-            license_resp = requests.get(response.json()['license']['url']).json() if response.json()['license'][
-                'url'] else None
-            return License(license_resp['name'], license_resp['permissions'], license_resp['conditions'],
-                           license_resp['limitations']) if license_resp else None
+            license_resp = requests.get(response.json()['license']['url']).json() if response.json()['license']['url'] else None
+            return License(license_resp['name'], license_resp['permissions'], license_resp['conditions'],license_resp['limitations']) if license_resp else None
         elif response.status_code == 404:
             return None
         else:
@@ -114,15 +111,16 @@ class GitHub(Git, Repository):
 
 class GitLab(Git, Repository):
 
+    def __init__(self, name, owner, url):
+        repository_license = self.get_repository_license(owner, name)
+        super().__init__(repository_license, name, owner, url)
+
     def get_repository_license(self, repository_owner, repository_name):
-        response = requests.get(f'https://gitlab.com/api/v4/projects/{repository_owner}%2F{repository_name}',
-                                params={'license': True})
+
+        response = requests.get(f'https://gitlab.com/api/v4/projects/{repository_owner}%2F{repository_name}', params={'license': True})
         if response.status_code == 200:
-            license_resp = requests.get(
-                f'https://gitlab.com/api/v4/licenses/{response.json()["license"]["key"]}').json() if response.json()[
-                'license'] else None
-            return License(license_resp['name'], license_resp['permissions'], license_resp['conditions'],
-                           license_resp['limitations']) if license_resp else None
+            license_resp = requests.get(f'https://gitlab.com/api/v4/licenses/{response.json()["license"]["key"]}').json() if response.json()['license'] else None
+            return License(license_resp['name'], license_resp['permissions'], license_resp['conditions'], license_resp['limitations']) if license_resp else None
         elif response.status_code == 404:
             return None
         else:
--- legal_check.py
+++ legal_check.py
@@ -6,21 +6,22 @@ from filemanager import File_Manager
 
 class License_Checker(File_Manager):
 
-    def __init__(self, path_to_lib) -> None:
+    def __init__(self, path_to_lib, is_local_search):
+        self.is_local_search = is_local_search
         File_Manager.__init__(File_Manager, path_to_lib)
 
-    def main(self, local):
-        if local:
+    def main(self):
+        if self.is_local_search:
             print("Find Local licenses")
-            for lic_path in checker.find_license_files():
-                rep = Repository(License.compare_license(lic_path), name=lic_path.split('/')[-2])
-                rep.print_license_data(rep)
+            for lic_path in self.find_license_files():
+                repository = Repository(License.analize_license(lic_path), name=lic_path.split('/')[-2])
+                Repository.print_repository_data(repository)
+
         else:
             print("Get Remote licenses")
             self.get_repos_info()
-            for repos in self.repositories:
-                repos.rep_license = repos.get_repository_license(repos.owner,repos.name)
-                repos.print_license_data(repos)
+            for repository in self.repositories:
+                Repository.print_repository_data(repository)
 
 
 if __name__ == '__main__':
@@ -29,8 +30,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='legal_check', usage='%(prog)s [options] libs separated by spaces')
     parser.add_argument('lib', help='Path to libraries folder')
     parser.add_argument('-l', action='store_true', help='Get a local license file')
-    args = parser.parse_args(['/Users/klaus/Downloads/test/legal_check/', '-l'])
+    args = parser.parse_args()
     
     if os.path.isdir(args.lib):
-        checker = License_Checker(args.lib)
-        checker.main(args.l)
+        checker = License_Checker(args.lib, args.l)
+        checker.main()
